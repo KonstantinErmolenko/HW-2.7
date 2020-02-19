bfee1e80cb01f79ab3de041f1e0d785c1c664e49
Konstantin Ermolenko <ermolenko.konst@yandex.ru>
2020-02-19 20:01:17 +0300

    Initial Commit

---
+++ HW 2.7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW 2.7
+//
+//  Created by Ермоленко Константин on 19.02.2020.
+//  Copyright © 2020 Ермоленко Константин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
